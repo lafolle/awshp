dc57824440f5599316110a3a6ce102a1f51f666a
lafolle <fghjklp@gmail.com>
2016-05-05 18:04:39 +0530

    code refactor

--- main.go
+++ main.go
@@ -6,37 +6,70 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/opsworks"
-	"github.com/lafolle/goodhosts"
+	"github.com/lafolle/etchosts"
+	"net"
+	"os"
 )
 
 var (
 	region  string
 	stackId string
+	dryRun  bool
 )
+
+type Instance struct {
+	Status   *string
+	Hostname *string
+	PublicIp *string
+}
 
 func init() {
 	flag.StringVar(&stackId, "stackId", "", "stackId")
 	flag.StringVar(&region, "region", "", "region")
+	flag.BoolVar(&dryRun, "dryRun", false, "do not commit changes")
 }
 
-func populateHostsFile(instanceMap map[*string]map[*string]*string) {
-	hosts, err := goodhosts.NewHosts()
+func populateHostsFile(instanceMap map[string]Instance) error {
+	hosts, err := etchosts.New("")
 	if err != nil {
-		fmt.Println("failed to get hosts")
-		return
+		return err
 	}
-	for _, imap := range instanceMap {
-		for hostname, ip := range imap {
-			if hosts.Has(*ip, *hostname) {
-				fmt.Println("host already has ", *ip, *hostname)
-				continue
+	for hostname, instance := range instanceMap {
+		entry, err := hosts.Read(hostname)
+		if err == nil {
+			if instance.PublicIp != nil && *instance.PublicIp != entry.Ipaddr.String() {
+				fmt.Println("action: update")
+				if err := hosts.Update(etchosts.Entry{
+					Hostname: hostname,
+					Ipaddr:   net.ParseIP(*instance.PublicIp),
+				}); err != nil {
+					fmt.Println("fail: action update", err)
+				}
 			}
-			hosts.Add(*ip, *hostname, "")
+			if instance.PublicIp == nil {
+				fmt.Println("action: delete")
+				if err := hosts.Delete(hostname); err != nil {
+					fmt.Println("fail: action delete", hostname)
+				}
+			}
+		} else if instance.PublicIp != nil {
+			fmt.Println("action: create")
+			if err := hosts.Create(etchosts.Entry{
+				Hostname: hostname,
+				Ipaddr:   net.ParseIP(*instance.PublicIp),
+			}); err != nil {
+				fmt.Println("fail: action create", err)
+			}
 		}
 	}
-	if err := hosts.Flush(); err != nil {
-		fmt.Println("failed to write to file", err)
+	if dryRun {
+		fmt.Println(hosts)
+	} else {
+		if err := hosts.Flush(); err != nil {
+			fmt.Println("failed to write to file", err)
+		}
 	}
+	return nil
 }
 
 func main() {
@@ -45,7 +78,8 @@ func main() {
 		flag.PrintDefaults()
 		return
 	}
-	var instanceMap map[*string]map[*string]*string = make(map[*string]map[*string]*string)
+	fmt.Println("dryRun:", dryRun)
+	var instanceMap map[string]Instance = make(map[string]Instance)
 	opsWorksInstance := opsworks.New(session.New(&aws.Config{Region: aws.String(region)}))
 	if opsWorksInstance == nil {
 		fmt.Println("Failed to get opsworks instance.")
@@ -66,16 +100,16 @@ func main() {
 			fmt.Println("failed to get instances for layer: ", layer.LayerId, err)
 			continue
 		}
-		instanceMap[layer.LayerId] = make(map[*string]*string)
 		for _, instance := range describeInstancesOutput.Instances {
-			if *instance.Status != *aws.String("online") {
-				fmt.Println("instance is offline (skipping)", *instance.Hostname)
-				continue
-			} else {
-				fmt.Println("instance is online", *instance.Hostname)
+			instanceMap[*instance.Hostname] = Instance{
+				Status:   instance.Status,
+				Hostname: instance.Hostname,
+				PublicIp: instance.PublicIp,
 			}
-			instanceMap[layer.LayerId][instance.Hostname] = instance.PublicIp
 		}
 	}
-	populateHostsFile(instanceMap)
+	if err := populateHostsFile(instanceMap); err != nil {
+		fmt.Println("fail to populate etchosts:", err)
+		os.Exit(1)
+	}
 }
